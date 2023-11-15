0d97e5d3c723b5f8ccbbffc96bf85addd8daf4bc
Livia Mendes <liviamendes1703@gmail.com>
2023-11-15 10:43:09 -0300

    user arraylist, objeto user, email, deletar salvar usuario, email e senha em arraylists diferentes -> criação de objeto user que salva tudo junto em uma única arraylist criação do método deletarUsuario adição do método isUsuario email, que verifica a existência de um usuário através do email (no caso do usuário ter esquecido seu nome de usuário) adição no método conectarEmail, que permite login por meio do email criação da classe User

--- login.java
+++ login.java
@@ -4,41 +4,38 @@
  * @version 14.11.2023
  */
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class login {
-
+    private Scanner entrada = new Scanner(System.in);
     private String usuario;
     private int senha;
-    /*private String[] banco_usuarios = new String[50];
-    private int[] banco_senhas = new int[50];*/
-    private ArrayList<String> arrUsuarios = new ArrayList<String>();
+    private String endEmail;
+    /*private ArrayList<String> arrUsuarios = new ArrayList<String>();
     private ArrayList<Integer> arrSenhas = new ArrayList<Integer>();
+    private ArrayList<String> arrEmails = new ArrayList<String>();*/
+    private ArrayList<User> arrUsers = new ArrayList<User>();
 
-    public login(){ /*construtor */
+    public login() { /*construtor */
 
     }
 
-    public String cadastrar(String username, int password) {
+    public String cadastrar(String username, int password, String email) {
         usuario = username;
         senha = password;
+        endEmail = email;
         boolean valido = true;
         String retorno = "";
         if (getDisponibilidade() > 0) { //checa se há espaço no banco de usuários
-                if (isUsuario(username)) { //checa se o nome de usuário está disponível para cadastro
+                if (isUsuario(username)||isUsuarioEmail(email)) { //checa se o nome de usuário está disponível para cadastro
                     valido = false;
                 }
             if (valido) {
-                /*for (int i = 0; i < banco_usuarios.length; i++) {
-                    if (banco_usuarios[i] == null) {
-                        banco_usuarios[i] = usuario; //guarda o usuario e sua senha no mesmo index de arrays diferentes
-                        banco_senhas[i] = senha;
-
-                        retorno = "Usuário cadastrado com sucesso";
-                    }
-                }*/
-                arrUsuarios.add(usuario);
-                arrSenhas.add(senha);
+                User user = new User(username, password, endEmail);
+                /*arrUsuarios.add(user.getUsuario());
+                arrEmails.add(user.getEmail());
+                arrSenhas.add(user.getSenha());*/
+                arrUsers.add(user);
             } else {
                 retorno = "Nome de usuário indisponível.";
             }
@@ -48,12 +45,7 @@ public class login {
     
     public int getDisponibilidade() { //verifica número de espaços disponíveis no banco
         int disponibilidade = 0;
-        /*for (int i = 0; i < banco_usuarios.length; i++) {
-            if (banco_usuarios[i] == null) {
-                disponibilidade++;
-            }
-        }*/
-        for(String user : arrUsuarios){
+        for(User user : arrUsers){
              if (user == null) {
                 disponibilidade++;
             }
@@ -63,29 +55,47 @@ public class login {
 
     public boolean isUsuario(String username) { //verifica se um nome de usuário está cadastrado no sistema
         boolean existe = false;
-        for (String name : arrUsuarios) {
-            if (name.equals(username)) {
+        for (User user : arrUsers) {
+            if (user.getUsuario().equals(username)) {
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    public boolean isUsuarioEmail(String email) { //verifica se um email está cadastrado no sistema
+        boolean existe = false;
+        for (User user : arrUsers) {
+            if (user.getEmail().equals(email)) {
                 existe = true;
             }
         }
         return existe;
     }
     
-    public String conectar(String username, int password) { //conecta o usuário ao sistema
+    public String conectarUsuario(String username, int password) { //conecta o usuário ao sistema por meio do nome de usuário
         usuario = username;
         senha = password;
         boolean match = false;
         String retorno = "";
 
-        if (isUsuario(username)) {
-            /*for (int i = 0; i < banco_usuarios.length; i++) {
-                for (int j = 0; j < banco_senhas.length; j++) {
-                    if (banco_usuarios[i].equals(username) && banco_senhas[j] == senha) {
-                        match = true;
-                    }
+        if(isUsuario(username)){
+            for (User user : arrUsers) {
+                if (user.getUsuario().equals(username) && user.getSenha() == senha) {
+                    match = true;
                 }
-            }*/
-            if(arrUsuarios.indexOf(username)==arrSenhas.indexOf(senha)){
+            }
+            if (match) {
+                retorno = "Usuário conectado";
+            } else {
+                retorno = "Senha incorreta";
+            }
+        }else {
+            retorno = "Usuário não encontrado";
+        }
+
+        /*if (isUsuario(username)) {
+            if (user.getUsuario()==username) {
                 match = true;
             }
             if (match) {
@@ -95,12 +105,64 @@ public class login {
             }
         } else {
             retorno = "Usuário não encontrado";
+        }*/
+
+        return retorno;
+    }
+    
+    public String conectarEmail(String email, int password) { //conecta usuário ao sistema por meio do email
+        endEmail = email;
+        senha = password;
+        boolean match = false;
+        String retorno = "";
+
+        if(isUsuarioEmail(endEmail)){
+            for (User user : arrUsers) {
+                if (user.getEmail().equals(endEmail) && user.getSenha() == senha) {
+                    match = true;
+                }
+            }
+            if (match) {
+                retorno = "Usuário conectado";
+            } else {
+                retorno = "Senha incorreta";
+            }
+        }else {
+            retorno = "Usuário não encontrado";
         }
+
+        /*if (isUsuarioEmail(endEmail)) {
+            if (arrUsuarios.indexOf(endEmail) == arrSenhas.indexOf(senha)) {
+                match = true;
+            }
+            if (match) {
+                retorno = "Usuário conectado";
+            } else {
+                retorno = "Senha incorreta";
+            }
+        } else {
+            retorno = "Usuário não encontrado";
+        }*/
 
         return retorno;
     }
 
-    public void deletarUsuario() { //deleta um usuário do sistema
+    public void deletarUsuario(String username) { //deleta um usuário do sistema
+        if (isUsuario(username)) {
+            System.out.println("digite sua senha:"); //verificação de senha
+            int senhaAux = entrada.nextInt();
+            for (User user : arrUsers) {
+                if (user.getUsuario().equals(username) && user.getSenha() == senhaAux) {
+                    System.out.println("tem certeza de que deseja deletar esta conta? Digite Sim para confirmar ou Não para cancelar");
+                    String resposta = entrada.nextLine();
+                    if (resposta.equals("sim") || resposta.equals("Sim") || resposta.equals("SIM")) {
+                        arrUsers.remove(user);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
     }
     
     public void alterarSenha() { //altera a senha de um usuário
