c2707ca44622b1b2769647bccc5a2bab6ff10f08
Livia Mendes <liviamendes1703@gmail.com>
2023-11-16 18:11:32 -0300

    finished: alterarSenha e alterarUsuario deleted unused private variables in login.java

--- testes.java
+++ testes.java
@@ -1,15 +1,11 @@
 public class testes {
     public static void main(String[] args) {
         login log1 = new login();
-
+        
         System.out.println(log1.cadastrar("livia", 152, "liviamendes@gmail.com")); //testado; funcionando
-
+        
         System.out.println(log1.isUsuario("livia")); //testado; funcionando
-        System.out.println(log1.isUsuario("mendes"));
+        
 
-        System.out.println(log1.isUsuarioEmail("liviamendes@gmail.com")); //testado; funcionando
-        System.out.println(log1.isUsuarioEmail("mendes@gmail.com"));
-
-        System.out.println(log1.conectarUsuario("livia", 152));
     }
 }
